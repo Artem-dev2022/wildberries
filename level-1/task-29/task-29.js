bc1d856c016a327626a782b0499e8b0defe55e3f
Artem <artem-agayan@mail.ru>
2023-08-21 18:15:55 +0300

    corrected form submit

--- level-1/task-29/task-29.js
+++ level-1/task-29/task-29.js
@@ -4,21 +4,21 @@
 // например, отправляет их на сервер 
 // или отображает всплывающее окно с результатами.
 
+const form = document.querySelector('form')
 const inputName = document.getElementById('name')
 const inputEmail = document.getElementById('email')
 const inputRadio = document.querySelectorAll('[type=radio]')
 const inputCheck = document.getElementById('check')
-const btn = document.getElementById('btn')
 
 function collectData(e) {
     e.preventDefault()
     const result = {
         name: inputName.value,
         mail: inputEmail.value,
-        color: [...inputRadio].find(i => i.checked).id,
+        color: [...inputRadio].find(i => i.checked).value,
         save: inputCheck.checked
     }
     alert(JSON.stringify(result))
 }
 
-btn.addEventListener('click', collectData)
+form.addEventListener('submit', collectData)
